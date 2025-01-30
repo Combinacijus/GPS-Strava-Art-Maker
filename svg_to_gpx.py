864278b63d9b32276191c2119b6aff986a7fa470
Combinacijus <combinacijus@gmail.com>
2025-01-30 19:20:16 +0200

    svg_to_path proof. And svg_to_gpx refactoring. drawing.svg example. gitignore

--- svg_to_gpx.py
+++ svg_to_gpx.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
                            QPushButton, QFileDialog, QHBoxLayout, QSlider, QLabel)
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWebChannel import QWebChannel
-from PyQt5.QtCore import Qt, QRectF, pyqtSlot, QObject, QStandardPaths
+from PyQt5.QtCore import Qt, QRectF, pyqtSlot, QObject, QStandardPaths, pyqtSignal
 from PyQt5.QtSvg import QSvgRenderer
 from PyQt5.QtGui import QPainter, QRegion
 
@@ -16,33 +16,17 @@ class SvgOverlay(QWidget):
         self.dragging = False
         self.last_pos = None
         self.setMouseTracking(True)
-        
-        # Make the widget transparent and initially ignore mouse events
         self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_TranslucentBackground)
         
     def setSvg(self, path):
         self.svg_renderer = QSvgRenderer(path)
-        self.adjustInitialScale()
         self.setAttribute(Qt.WA_TransparentForMouseEvents, False)
         self.updateMask()
         self.show()
         self.update()
         
-    def adjustInitialScale(self):
-        if self.svg_renderer:
-            svg_size = self.svg_renderer.defaultSize()
-            widget_size = self.size()
-            width_scale = (widget_size.width() / 3) / svg_size.width()
-            height_scale = (widget_size.height() / 3) / svg_size.height()
-            self.scale = min(width_scale, height_scale)
-            self.position = [
-                widget_size.width() / 2 - (svg_size.width() * self.scale) / 2,
-                widget_size.height() / 2 - (svg_size.height() * self.scale) / 2
-            ]
-            self.updateMask()
-    
     def updateMask(self):
         if self.svg_renderer:
             svg_size = self.svg_renderer.defaultSize()
@@ -97,13 +81,47 @@ class SvgOverlay(QWidget):
             self.last_pos = event.pos()
             self.updateMask()
             self.update()
+            self.parent().updateSVGAnchorFromPosition(self.position)
             event.accept()
         else:
             event.ignore()
 
+class Bridge(QObject):
+    updateScale = pyqtSignal(float)
+    
+    def __init__(self, map_widget):
+        super().__init__()
+        self.map_widget = map_widget
+
+    @pyqtSlot(float, float, int)
+    def updateMapCenter(self, lat, lng, zoom):
+        print(f"Map center: {lat}, {lng}, zoom: {zoom}")
+        if hasattr(self.map_widget.main_window, 'initial_zoom') and hasattr(self.map_widget.main_window, 'initial_scale'):
+            current_zoom = zoom
+            scale_factor = 2 ** (current_zoom - self.map_widget.main_window.initial_zoom)
+            new_scale = self.map_widget.main_window.initial_scale * scale_factor
+            self.map_widget.svg_overlay.scale = new_scale
+            self.updateScale.emit(new_scale)
+            self.map_widget.svg_overlay.updateMask()
+            self.map_widget.svg_overlay.update()
+
+    @pyqtSlot(float, float)
+    def updateSVGPosition(self, x, y):
+        svg_overlay = self.map_widget.svg_overlay
+        if svg_overlay.svg_renderer:
+            svg_size = svg_overlay.svg_renderer.defaultSize()
+            scaled_width = svg_size.width() * svg_overlay.scale
+            scaled_height = svg_size.height() * svg_overlay.scale
+            pos_x = x - scaled_width / 2
+            pos_y = y - scaled_height / 2
+            svg_overlay.position = [pos_x, pos_y]
+            svg_overlay.updateMask()
+            svg_overlay.update()
+
 class MapWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, main_window, parent=None):
         super().__init__(parent)
+        self.main_window = main_window
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         
@@ -114,8 +132,9 @@ class MapWidget(QWidget):
         self.svg_overlay.resize(self.size())
         
         self.channel = QWebChannel()
-        self.page = self.map_view.page()
-        self.page.setWebChannel(self.channel)
+        self.bridge = Bridge(self)
+        self.channel.registerObject('qtObject', self.bridge)
+        self.map_view.page().setWebChannel(self.channel)
         
         self.map_view.setHtml('''
             <!DOCTYPE html>
@@ -135,6 +154,40 @@ class MapWidget(QWidget):
                     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                         attribution: '© OpenStreetMap contributors'
                     }).addTo(map);
+                    
+                    var qtObject = {
+                        updateMapCenter: function(lat, lng, zoom) {},
+                        updateSVGPosition: function(x, y) {}
+                    };
+                    
+                    new QWebChannel(qt.webChannelTransport, function(channel) {
+                        qtObject = channel.objects.qtObject;
+                    });
+                    
+                    var svgAnchor = null;
+                    
+                    function sendMapCenter() {
+                        var center = map.getCenter();
+                        var zoom = map.getZoom();
+                        qtObject.updateMapCenter(center.lat, center.lng, zoom);
+                    }
+                    
+                    function updateSVGPosition() {
+                        if (svgAnchor) {
+                            var point = map.latLngToContainerPoint(svgAnchor);
+                            qtObject.updateSVGPosition(point.x, point.y);
+                        }
+                    }
+                    
+                    map.on('move', function() {
+                        sendMapCenter();
+                        updateSVGPosition();
+                    });
+                    
+                    map.on('zoom', function() {
+                        sendMapCenter();
+                        updateSVGPosition();
+                    });
                 </script>
             </body>
             </html>
@@ -143,6 +196,15 @@ class MapWidget(QWidget):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         self.svg_overlay.resize(self.size())
+    
+    def updateSVGAnchorFromPosition(self, position):
+        x = position[0] + (self.svg_overlay.svg_renderer.defaultSize().width() * self.svg_overlay.scale) / 2
+        y = position[1] + (self.svg_overlay.svg_renderer.defaultSize().height() * self.svg_overlay.scale) / 2
+        js_code = f'''
+            var point = L.point({x}, {y});
+            svgAnchor = map.containerPointToLatLng(point);
+        '''
+        self.map_view.page().runJavaScript(js_code)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -155,7 +217,7 @@ class MainWindow(QMainWindow):
         main_layout = QVBoxLayout(central_widget)
         main_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.map_widget = MapWidget()
+        self.map_widget = MapWidget(self)
         main_layout.addWidget(self.map_widget)
 
         controls_layout = QHBoxLayout()
@@ -176,6 +238,8 @@ class MainWindow(QMainWindow):
         controls_layout.addWidget(self.scale_slider)
         
         main_layout.addLayout(controls_layout)
+        
+        self.map_widget.bridge.updateScale.connect(self.update_slider_from_scale)
 
     def load_svg(self):
         downloads_path = QStandardPaths.writableLocation(QStandardPaths.DownloadLocation)
@@ -185,15 +249,44 @@ class MainWindow(QMainWindow):
             downloads_path,
             "SVG Files (*.svg)"
         )
-        
         if file_name:
-            self.map_widget.svg_overlay.setSvg(file_name)
-    
+            self.map_widget.map_view.page().runJavaScript(
+                "map.getCenter().lat + ',' + map.getCenter().lng + ',' + map.getZoom()",
+                lambda result: self.on_map_data_retrieved(result, file_name)
+            )
+
+    def on_map_data_retrieved(self, result, file_name):
+        parts = result.split(',')
+        if len(parts) != 3:
+            return
+        lat, lng, zoom = parts
+        self.initial_zoom = int(zoom)
+        self.initial_scale = 1.0  # Initial scale set to 100%
+        self.map_widget.svg_overlay.setSvg(file_name)
+        self.scale_slider.setValue(int(self.initial_scale * 100))
+        js_code = f"svgAnchor = L.latLng({lat}, {lng});"
+        self.map_widget.map_view.page().runJavaScript(js_code)
+
     def update_scale(self, value):
-        if self.map_widget.svg_overlay.svg_renderer:
-            self.map_widget.svg_overlay.scale = value / 100.0
-            self.map_widget.svg_overlay.updateMask()
-            self.map_widget.svg_overlay.update()
+        if hasattr(self, 'initial_zoom'):
+            self.initial_scale = value / 100.0
+            self.map_widget.map_view.page().runJavaScript(
+                "map.getZoom()",
+                lambda zoom: self.update_scale_based_on_zoom(zoom)
+            )
+
+    def update_scale_based_on_zoom(self, zoom):
+        current_zoom = int(zoom)
+        scale_factor = 2 ** (current_zoom - self.initial_zoom)
+        new_scale = self.initial_scale * scale_factor
+        self.map_widget.svg_overlay.scale = new_scale
+        self.map_widget.svg_overlay.updateMask()
+        self.map_widget.svg_overlay.update()
+    
+    def update_slider_from_scale(self, scale):
+        self.scale_slider.blockSignals(True)
+        self.scale_slider.setValue(int(scale * 100))
+        self.scale_slider.blockSignals(False)
 
 def main():
     app = QApplication(sys.argv)
